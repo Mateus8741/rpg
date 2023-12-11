88b12256b4e138ce2adbfa142a5f1edd84b0afff
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-11 16:26:29 -0300

    Update Header component styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,21 +7,21 @@ interface Persona {
 export function Header({ persona }: Persona) {
   return (
     <div className="flex flex-col sm:flex-row justify-center items-center gap-3 bg-white text-black text-center py-4 px-4">
-      <div className="flex flex-col sm:flex-row gap-3 items-center">
+      <div className="flex flex-col sm:flex-row gap-1 items-center">
         <h2 className="text-2xl font-bold">Nome</h2>
         <h2 className="text-2xl font-normal border-2 border-black px-2">
           {persona.nome}
         </h2>
       </div>
 
-      <div className="flex flex-col sm:flex-row gap-3 items-center">
+      <div className="flex flex-col sm:flex-row gap-1 items-center">
         <p className="text-2xl font-bold">Level</p>
         <p className="text-2xl font-normal border-2 border-black px-2">
           {persona.level}
         </p>
       </div>
 
-      <div className="flex flex-col sm:flex-row gap-3 items-center">
+      <div className="flex flex-col sm:flex-row gap-1 items-center">
         <p className="text-2xl font-bold">Experiência</p>
         <p className="text-2xl font-normal border-2 border-black px-2">
           {persona.experiencia}
