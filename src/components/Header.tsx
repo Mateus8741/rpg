7f0f1334967b42c574c348d7f19bbc30c0cedfc8
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-11 19:13:31 -0300

    Update Header component styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,29 +8,37 @@ export function Header({ persona }: Persona) {
   return (
     <div className="flex flex-col sm:flex-row justify-center items-center gap-3 bg-gray-600 text-white text-center py-4 px-4">
       <div className="flex flex-row gap-1 items-center">
-        <h2 className="text-xs md:text-xl lg:text-xl font-bold">Nome</h2>
-        <h2 className="text-xl font-normal border-2 border-white px-2">
+        <h2 className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-bold">
+          Nome
+        </h2>
+        <h2 className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
           {persona.nome}
         </h2>
       </div>
 
       <div className="flex flex-row gap-1 items-center">
-        <p className="text-xs md:text-xl lg:text-xl font-bold">Level</p>
-        <p className="text-xl font-normal border-2 border-white px-2">
+        <p className="text-xs sm:text-sm md:text-xl lg:text-xl font-bold">
+          Level
+        </p>
+        <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
           {persona.level}
         </p>
       </div>
 
       <div className="flex flex-row gap-1 items-center">
-        <p className="text-xs md:text-xl lg:text-xl font-bold">Experiência</p>
-        <p className="text-xl font-normal border-2 border-white px-2">
+        <p className="text-xs sm:text-sm md:text-xl lg:text-xl font-bold">
+          Experiência
+        </p>
+        <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
           {persona.experiencia}
         </p>
       </div>
 
       <div className="flex flex-row gap-1 items-center">
-        <p className="text-xs md:text-2xl lg:text-xl font-bold">Gold</p>
-        <p className="text-xl font-normal border-2 border-white px-2">
+        <p className="text-xs sm:text-sm md:text-2xl lg:text-xl font-bold">
+          Gold
+        </p>
+        <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
           {persona.gold}
         </p>
       </div>
