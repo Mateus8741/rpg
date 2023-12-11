31cf5c06bb204c3c218ce2ff0f11bdf1a9f386fc
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-11 18:03:42 -0300

    Update Atributes and Header components

--- src/components/Atributes.tsx
+++ src/components/Atributes.tsx
@@ -1,5 +1,6 @@
 import { Personagem } from '@/models/rpgDTO'
 import Image from 'next/image'
+import armor from '../images/schemaArmor.png'
 
 interface AtributesProps {
   persona: Personagem
@@ -8,57 +9,8 @@ interface AtributesProps {
 export function Atributes({ persona }: AtributesProps) {
   return (
     <div className="flex flex-col mb-4">
-      <div className="grid grid-cols-3 gap-4 mb-4">
+      <div className="grid grid-cols-4 gap-4 mb-4">
         <table className="min-w-full">
-          {/* <h1 className="text-lg font-semibold">Atributos</h1> */}
-          <thead>
-            <tr>
-              <th colSpan={2} className="text-lg font-semibold text-center">
-                Atributos
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="bg-white divide-y divide-gray-200">
-            <tr className="text-left text-sm font-medium text-gray-900">
-              <td className="">HP:</td>
-              <td className="">{persona.atributos.hp}</td>
-            </tr>
-
-            <tr className="text-left text-sm font-medium text-gray-900">
-              <td className="">MP:</td>
-              <td className="">{persona.atributos.mp}</td>
-            </tr>
-
-            <tr className="text-left text-sm font-medium text-gray-900">
-              <td className="">FOR:</td>
-              <td className="">{persona.atributos.forca}</td>
-            </tr>
-
-            <tr className="text-left text-sm font-medium text-gray-900">
-              <td className="">AGL:</td>
-              <td className="">{persona.atributos.agilidade}</td>
-            </tr>
-
-            <tr className="text-left text-sm font-medium text-gray-900">
-              <td className="">DES:</td>
-              <td className="">{persona.atributos.destreza}</td>
-            </tr>
-
-            <tr className="text-left text-sm font-medium text-gray-900">
-              <td className="">CON:</td>
-              <td className="">{persona.atributos.constituicao}</td>
-            </tr>
-
-            <tr className="text-left text-sm font-medium text-gray-900">
-              <td className="">INT:</td>
-              <td className="">{persona.atributos.inteligencia}</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <table className="min-w-full">
-          {/* <h1 className="text-lg font-semibold">Atributos</h1> */}
           <thead>
             <tr>
               <th colSpan={2} className="text-lg font-semibold text-center">
@@ -137,12 +89,12 @@ export function Atributes({ persona }: AtributesProps) {
 
             <tr className="text-left text-sm font-medium text-gray-900">
               <td className="py-2">5:</td>
-              <td className="py-2">arma esquerda</td>
+              <td className="py-2">arma</td>
             </tr>
 
             <tr className="text-left text-sm font-medium text-gray-900">
               <td className="py-2">6:</td>
-              <td className="py-2">arma direita</td>
+              <td className="py-2">arma</td>
             </tr>
             {/* Adicionar outros atributos aqui */}
           </tbody>
@@ -170,13 +122,33 @@ export function Atributes({ persona }: AtributesProps) {
           </tbody>
         </table>
 
-        <div>
-          <Image
-            src="/images/placeholder.png"
-            width={100}
-            height={100}
-            alt=""
-          />
+        <div className="col-span-4 sm:col-span-2 lg:col-span-1">
+          <table className="min-w-full">
+            <thead>
+              <tr>
+                <th colSpan={2} className="text-lg font-semibold text-center">
+                  Ataque e Defesa
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <tr className="text-center">
+                <td colSpan={2} className="py-4" align="center">
+                  <Image src={armor} width={120} height={120} alt="Armor" />
+                </td>
+              </tr>
+
+              <tr className="text-left text-sm font-medium text-gray-900">
+                <td className="">Ataque Máx:</td>
+                <td className="">{persona.ataqueMaximo}</td>
+              </tr>
+
+              <tr className="text-left text-sm font-medium text-gray-900">
+                <td className="">Defesa Máx:</td>
+                <td className="">{persona.defesaMaxima}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
