3221f6469b911fb7caada5c4dd0994988e62abe5
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-11 21:14:09 -0300

    Refactor form layout and add case-insensitive search for persona

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
   const router = useRouter()
   const [username, setUsername] = useState('')
 
-  const persona = PersonaFixas.find((persona) => persona.nome === username)
+  const persona = PersonaFixas.find(
+    (persona) => persona.nome.toLowerCase() === username.toLocaleLowerCase(),
+  )
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault()
@@ -22,35 +24,45 @@ export default function Home() {
 
   return (
     <main className="min-h-screen text-black bg-white">
-      <form
-        className="flex flex-col items-center justify-center h-screen"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="text-6xl font-bold mb-12">Sign In</h1>
-        <div className="w-1/3">
-          <label
-            className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="username"
-          >
-            Nome do personagem
-          </label>
-          <input
-            className="w-full p-2 mb-6 text-lg bg-gray-100 border border-gray-400 rounded"
-            id="username"
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+      <div className="flex flex-col h-screen items-center justify-center">
+        <form
+          className="flex w-full items-center justify-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="w-1/3">
+            <label
+              className="block mb-2 text-xs font-bold uppercase"
+              htmlFor="username"
+            >
+              Nome do personagem
+            </label>
+            <input
+              className="w-full p-2 mb-6 text-lg bg-gray-100 border border-gray-400 rounded"
+              id="username"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
 
+            <button
+              className="flex items-center justify-center w-full p-4 mb-6 text-lg text-white bg-gray-900 rounded"
+              type="submit"
+            >
+              Ver personagem
+            </button>
+          </div>
+        </form>
+
+        <div className="w-1/3">
           <button
             className="flex items-center justify-center w-full p-4 mb-6 text-lg text-white bg-gray-900 rounded"
-            type="submit"
+            onClick={() => router.push('/personagens')}
           >
-            LogIn
+            Ver todos os personagens
           </button>
         </div>
-      </form>
+      </div>
     </main>
   )
 }
