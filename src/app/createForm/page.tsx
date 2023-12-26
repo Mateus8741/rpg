9ea1ef7f6bf6541c070b7ec00e941c8096154929
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-26 13:53:37 -0300

    Add react-hook-form dependency and update page layout

--- src/app/createForm/page.tsx
+++ src/app/createForm/page.tsx
@@ -1,3 +1,9 @@
 export default function CreateForm() {
-  return <div>page</div>
+  return (
+    <main className="min-h-screen text-black bg-white dark:bg-gray-900 dark:text-white">
+      <div className="flex flex-col h-screen items-center justify-center">
+        <div>page</div>
+      </div>
+    </main>
+  )
 }
