0bb66a27708e1bfac22110a002be979f7ee903e9
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-27 19:36:21 -0300

    Add edit functionality to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,56 @@
 import { Personagem } from '@/models/rpgDTO'
+import { FaPenToSquare } from 'react-icons/fa6'
 
 interface Persona {
   persona: Personagem
 }
 
 export function Header({ persona }: Persona) {
+  function handleEdit() {
+    console.log('edit')
+  }
+
   return (
-    <div className="flex flex-col sm:flex-row justify-center items-center gap-3 bg-gray-600 text-white text-center py-4 px-4">
-      <div className="flex flex-row gap-1 items-center">
-        <h2 className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-bold">
-          Nome
-        </h2>
-        <h2 className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
-          {persona.nome}
-        </h2>
+    <div className="flex flex-col sm:flex-row justify-center items-center bg-gray-600 text-white text-center py-4 px-4">
+      <div className="flex flex-1 flex-row gap-4">
+        <div className="flex flex-row gap-1 items-center">
+          <h2 className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-bold">
+            Nome
+          </h2>
+          <h2 className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
+            {persona.nome}
+          </h2>
+        </div>
+
+        <div className="flex flex-row gap-1 items-center">
+          <p className="text-xs sm:text-sm md:text-xl lg:text-xl font-bold">
+            Level
+          </p>
+          <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
+            {persona.level}
+          </p>
+        </div>
+
+        <div className="flex flex-row gap-1 items-center">
+          <p className="text-xs sm:text-sm md:text-xl lg:text-xl font-bold">
+            Experiência
+          </p>
+          <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
+            {persona.experiencia}
+          </p>
+        </div>
+
+        <div className="flex flex-row gap-1 items-center">
+          <p className="text-xs sm:text-sm md:text-2xl lg:text-xl font-bold">
+            Gold
+          </p>
+          <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
+            {persona.gold}
+          </p>
+        </div>
       </div>
 
-      <div className="flex flex-row gap-1 items-center">
-        <p className="text-xs sm:text-sm md:text-xl lg:text-xl font-bold">
-          Level
-        </p>
-        <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
-          {persona.level}
-        </p>
-      </div>
-
-      <div className="flex flex-row gap-1 items-center">
-        <p className="text-xs sm:text-sm md:text-xl lg:text-xl font-bold">
-          Experiência
-        </p>
-        <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
-          {persona.experiencia}
-        </p>
-      </div>
-
-      <div className="flex flex-row gap-1 items-center">
-        <p className="text-xs sm:text-sm md:text-2xl lg:text-xl font-bold">
-          Gold
-        </p>
-        <p className="text-xs sm:text-sm md:text-sm lg:text-md xl:text-xl font-normal border-2 border-white px-2">
-          {persona.gold}
-        </p>
-      </div>
+      <FaPenToSquare className="text-2xl cursor-pointer" onClick={handleEdit} />
     </div>
   )
 }
