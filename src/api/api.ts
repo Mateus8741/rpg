d96d6eb98f00e4772e93151f33c949265901bf28
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-30 18:28:10 -0300

    Refactor API module to use axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,42 +1,7 @@
 'use client'
 
-import { Personagem } from '@/models/rpgDTO'
+import axios from 'axios'
 
-export function api() {
-  const baseUrl = `http://localhost:3333/heroes`
-
-  const callApi = {
-    get: async () => {
-      const response = await fetch(baseUrl)
-      const data = await response.json()
-      return data
-    },
-    post: async (body: Personagem) => {
-      const response = await fetch(baseUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      })
-      const data = await response.json()
-      return data
-    },
-    put: async (body: Personagem) => {
-      const response = await fetch(baseUrl, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      })
-      const data = await response.json()
-      return data
-    },
-    delete: async () => {
-      const response = await fetch(baseUrl, { method: 'DELETE' })
-      const data = await response.json()
-      return data
-    },
-  }
-
-  const { get, post, put, delete: del } = callApi
-
-  return { get, post, put, del }
-}
+export const api = axios.create({
+  baseURL: 'https://rpg-api-production.up.railway.app',
+})
