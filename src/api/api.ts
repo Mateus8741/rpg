150509b5eb69f4617968854e9326cf528c5629b6
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-03 18:55:26 -0300

    Update API base URL

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,5 +3,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/heroes',
+  // baseURL: 'http://localhost:3333/heroes',
+  baseURL: 'https://db-rpg-vercel.vercel.app/heroes',
 })
