ce341fdd0f72bb3f360069f4e4813173a6379c06
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-08 11:44:04 -0300

    Refactor API route to use Prisma ORM in route.ts

--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -1,13 +1,29 @@
-import data from '@/server/db.json'
+// import data from '@/server/db.json'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export async function GET() {
-  return Response.json(data.heroes)
+  const heroes = await prisma.hero.findMany()
+
+  return Response.json(heroes)
 }
 
 export async function POST(request: Request) {
   const newHero = await request.json()
 
-  data.heroes.push(newHero)
+  const createdHero = await prisma.hero.create({ data: newHero })
 
-  return Response.json(newHero, { status: 201 })
+  if (!createdHero) {
+    return Response.json('Error creating hero', { status: 500 })
+  }
+
+  return Response.json(createdHero, { status: 201 })
+
+  // const newHero = await request.json()
+
+  // data.heroes.push(newHero)
+
+  // return Response.json(newHero, { status: 201 })
 }
